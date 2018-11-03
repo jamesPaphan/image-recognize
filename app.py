aeb404877204879e37979c3c0478f23756f2a177
James Paphan <paphan.c@ku.th>
2018-11-04 00:54:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def predict():
         # print('Prediction :{}, confidence : {:.3f}'.format(
         #     pred_label_test,
         #     values[0][pred_class_test]))
-    return pred_label_test
+    return json.dumps({'label': pred_class_test, 'confidence': values})
 
 if __name__ == '__main__':
     # print(os.listdir())
