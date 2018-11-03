ddb93163c3f58ac8b5e0aff58732d99a5e345d04
James Paphan <paphan.c@ku.th>
2018-11-03 17:03:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,36 +16,37 @@ def index():
     #sending a hello back to the requester
     return "Hello " + name
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     frozen_graph_filename = 'tensorflow_inception_graph.pd'
-#     with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
-#         graph_def = tf.GraphDef()
-#         graph_def.ParseFromString(f.read())
-#
-#     graph = tf.get_default_graph()
-#     tf.import_graph_def(graph_def, name='')
-#
-#     batch = graph.get_tensor_by_name('input:0')
-#     prediction = graph.get_tensor_by_name('output:0')
-#
-#     with tf.Session() as sess:
-#         img_path = 'test_img.png'
-#         img = image.load_img(img_path, target_size=(224, 224))
-#         x = image.img_to_array(img)
-#         x = np.expand_dims(x, axis=0)
-#         x = preprocess_input(x)
-#
-#         values = sess.run(prediction, feed_dict={batch: x})
-#
-#         pred_class_test = np.argmax(values)
-#         pred_label_test = idx2label[pred_class_test]
-#         print('Prediction :{}, confidence : {:.3f}'.format(
-#             pred_label_test,
-#             values[0][pred_class_test]))
-#
-#         plt.imshow(img)
-#         plt.show()
+@app.route('/predict', methods=['POST'])
+def predict():
+    frozen_graph_filename = 'tensorflow_inception_graph.pd'
+    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+    graph = tf.get_default_graph()
+    tf.import_graph_def(graph_def, name='')
+
+    batch = graph.get_tensor_by_name('input:0')
+    prediction = graph.get_tensor_by_name('output:0')
+
+    x = request.form['features']
+    return x
+
+    # with tf.Session() as sess:
+    #     img_path = 'test_img.png'
+    #     img = image.load_img(img_path, target_size=(224, 224))
+    #     x = image.img_to_array(img)
+    #     x = np.expand_dims(x, axis=0)
+    #     x = preprocess_input(x)
+    #
+    #     values = sess.run(prediction, feed_dict={batch: x})
+    #
+    #     pred_class_test = np.argmax(values)
+    #     pred_label_test = idx2label[pred_class_test]
+    #     print('Prediction :{}, confidence : {:.3f}'.format(
+    #         pred_label_test,
+    #         values[0][pred_class_test]))
+
 
 if __name__ == '__main__':
     app.run()
