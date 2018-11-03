72b096fe75f847ff0c67f19271e6ca46ad901e69
James Paphan <paphan.c@ku.th>
2018-11-03 18:04:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,11 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # frozen_graph_filename = 'tensorflow_inception_graph.pd'
-    # with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
-    #     graph_def = tf.GraphDef()
-    #     graph_def.ParseFromString(f.read())
-    #
+    frozen_graph_filename = 'tensorflow_inception_graph.pd'
+    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
     # graph = tf.get_default_graph()
     # tf.import_graph_def(graph_def, name='')
     #
