35664784156f5a83b7749a6334177ca3c318c647
James Paphan <paphan.c@ku.th>
2018-11-03 22:23:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import tensorflow as tf
+import numpy as np
 import os
 
 app = Flask(__name__)
@@ -30,26 +31,23 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     x = request.get_json()['features']
-    return x
 
-    # with tf.Session() as sess:
-    #     img_path = 'test_img.png'
-    #     img = image.load_img(img_path, target_size=(224, 224))
-    #     x = image.img_to_array(img)
-    #     x = np.expand_dims(x, axis=0)
-    #     x = preprocess_input(x)
-    #
-    #     values = sess.run(prediction, feed_dict={batch: x})
-    #
-    #     pred_class_test = np.argmax(values)
-    #     pred_label_test = idx2label[pred_class_test]
-    #     print('Prediction :{}, confidence : {:.3f}'.format(
-    #         pred_label_test,
-    #         values[0][pred_class_test]))
+    with tf.Session(graph=graph) as sess:
+        x = np.expand_dims(x, axis=0)
+        values = sess.run(prediction, feed_dict={batch: x})
 
+        pred_class_test = np.argmax(values)
+        # pred_label_test = idx2label[pred_class_test]
+        # print('Prediction :{}, confidence : {:.3f}'.format(
+        #     pred_label_test,
+        #     values[0][pred_class_test]))
+    return pred_class_test
 
 if __name__ == '__main__':
-    print(os.listdir())
-    frozen_graph_filename = './models/vgg16_edit.pb'
+    # print(os.listdir())
+    frozen_graph_filename = './models/tensorflow_inception_graph.pb'
     graph = load_graph(frozen_graph_filename)
+    batch = graph.get_tensor_by_name('input:0')
+    prediction = graph.get_tensor_by_name('output:0')
+
     app.run()
