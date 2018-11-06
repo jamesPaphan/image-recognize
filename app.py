8c75f8a4b4536687b152071b25731f291d0cf115
James Paphan <paphan.c@ku.th>
2018-11-06 19:35:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import os
 import json
 from keras.models import load_model
 
-from keras.preprocessing import image
-
 app = Flask(__name__)
 
 def load_graph(frozen_graph_filename):
@@ -30,18 +28,6 @@ labels = f.read()
 labels = labels.split('\n')
 f.close()
 
-
-# img_path = './models/test_img.png'
-# img = image.load_img(img_path, target_size=(224, 224))
-# xx = image.img_to_array(img)
-
-# y = xx.tolist()
-# my_json_string = json.dumps(y)
-#
-# tmp = np.reshape(y,(-1))
-# my_json_string = json.dumps(tmp.tolist())
-
-
 @app.route('/')
 def hello_world():
     return 'Hello World'
@@ -59,8 +45,6 @@ def index():
 def predict():
     data = request.get_json()['features']
     x = json.loads(data)
-    # x = json.loads(my_json_string)
-    # text = json.loads(data)
 
     with tf.Session(graph=graph) as sess:
         x = np.reshape(x,(224,224,3))
@@ -73,7 +57,6 @@ def predict():
         #     pred_label_test,
         #     values[0][pred_class_test]))
     return json.dumps({'label': pred_label_test, 'confidence': str(np.max(values))})
-    # return pred_label_test + ' ' + str(text)
 
 if __name__ == '__main__':
     # print(os.listdir())
